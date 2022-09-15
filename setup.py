0b0450dbd06aba08d275714f68506dcad5735925
osi1880vr <87379616+osi1880vr@users.noreply.github.com>
2022-09-15 11:34:26 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pytorch3d-lite',
+    version='2.1.0',
+    description='lite version of pytorch3d with just needed functions to run disco diffusion v5 and above',
+    packages=find_packages(),
+    install_requires=[],
+)
